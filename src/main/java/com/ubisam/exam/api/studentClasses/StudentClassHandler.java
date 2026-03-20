3f98af00c4266d2d9cb28336bf75eb0afef52ce9
조흥재 연구원 <heung@ubisam.com>
2026-03-20 18:10:58 +0900

    20260320-쿼리 수정

--- src/main/java/com/ubisam/exam/api/studentClasses/StudentClassHandler.java
+++ src/main/java/com/ubisam/exam/api/studentClasses/StudentClassHandler.java
@@ -2,6 +2,7 @@ package com.ubisam.exam.api.studentClasses;
 
 import java.io.Serializable;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleAfterDelete;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import com.ubisam.exam.domain.StudentClass;
 
+import io.u2ware.common.data.jpa.repository.query.JpaSpecificationBuilder;
 import io.u2ware.common.data.rest.core.annotation.HandleAfterRead;
 import io.u2ware.common.data.rest.core.annotation.HandleBeforeRead;
 
@@ -21,11 +23,17 @@ import io.u2ware.common.data.rest.core.annotation.HandleBeforeRead;
 public class StudentClassHandler {
 
   @HandleBeforeRead
-  public void beforeRead(StudentClass e, Serializable r) throws Exception{
+  public void beforeRead(StudentClass e, Specification spec) throws Exception{
     //로그 코드 작성 (Auditing)
     //테스트에서는 sysout으로 작성하나 실제는 log 사용
     System.out.println("[HandlebeforeRead] "+e);
-    System.out.println("[HandlebeforeRead] "+r);
+    JpaSpecificationBuilder<StudentClass> query = JpaSpecificationBuilder.of(StudentClass.class);
+    // select * from where stCount = 키워드 or name = '키워드'
+    query.where()
+        .and().eq("className", e.getKeyword())
+        .or().eq("stCount", Integer.valueOf(e.getKeyword()))
+        .build(spec);
+
   }
 
   @HandleAfterRead
